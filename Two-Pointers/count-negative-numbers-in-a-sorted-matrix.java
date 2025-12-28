70f3c8b20b83f6a79fa8460e65c96ffd64d92e28
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2025-12-28 22:27:36 +0530

    Update count-negative-numbers-in-a-sorted-matrix.java
    reducing runtime

--- Two-Pointers/count-negative-numbers-in-a-sorted-matrix.java
+++ Two-Pointers/count-negative-numbers-in-a-sorted-matrix.java
@@ -1,16 +1,22 @@
 class Solution {
     public int countNegatives(int[][] grid) {
         int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=grid[0].length-1;j>=0;j--){
-                if(grid[i][j]>0){
-                    break;
-                }
-                else if(grid[i][j]<0){
-                    count++;
-                }
+        int len=grid.length-1; //3
+        int intlen=grid[0].length-1; //3
+        int t1=len;
+        int t2=intlen;
+        for(int i=(len+1)*(intlen+1)-1;i>=0;i--){
+            if (t1<0){
+                break;
             }
-
+            if (t2<0){
+                t1--;
+                t2=intlen;
+            }
+            if(grid[t1][t2]<0){
+                count++;
+            }
+            t2--;
         }
         return count;
     }
