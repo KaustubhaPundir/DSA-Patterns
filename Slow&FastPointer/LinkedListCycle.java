58290f023d9c3d9fc397efd1850178e9a7bdcdba
Kaustubha_Pundir <kaustubhapundir@gmail.com>
2026-01-02 22:20:08 +0530

    Fix formatting issues in LinkedListCycle.java

--- Slow&FastPointer/LinkedListCycle.java
+++ Slow&FastPointer/LinkedListCycle.java
@@ -2,7 +2,6 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
-
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
